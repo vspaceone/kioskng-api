b4468ebbee1b959682009bfbf2040cafff68552f
Damian Jesionek <damian@jesionek.dev>
2023-04-10 19:39:08 +0200

    Added missing media-mappings validation code

--- src/media-mappings/app.js
+++ src/media-mappings/app.js
@@ -1,10 +1,48 @@
 'use strict';
 
 const { DynamoRestfulHandler } = require('./DynamoRestfulHandler.js');
+const Ajv = require('ajv');
 
+const region = "eu-north-1";
 const tableName = "media-mappings";
 
 exports.handler = async (event, context) => {
-    const restHandler = new DynamoRestfulHandler(tableName);
+    const restHandler = new DynamoRestfulHandler(region, tableName, new PayloadValidators());
     return await restHandler.handleApiEvent(event);
 };
+
+class PayloadValidators {
+
+    constructor(){
+        this.ajv = new Ajv();
+    }
+
+    generatePutMediaMappingValidator() {
+        return this.ajv.compile({
+            type: "object",
+            properties: {
+                // id not allowed
+                media_identification: {
+                    type: "string"
+                },
+                account_id: {
+                    type: "string", 
+                    pattern: "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$" // = UUID
+                },
+                media_type: {
+                    enum: ["RFID_ID", "QR_CODE_EXACT_MATCH", "APP_NFC", "APP_TOKEN"]
+                },
+                device_type: {
+                    enum: ["App", "PoS"]
+                },
+                device_name: {
+                    type: "string"
+                },
+                valid_till_timestamp: {
+                    type: "integer"
+                }
+            },
+            required: ["media_identification", "account_id", "media_type"]
+        })
+    }
+}
