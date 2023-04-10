9f2fdbbaf59718a53b5385c4d027af397f82dbb9
Damian Jesionek <damian@jesionek.dev>
2023-04-10 19:35:25 +0200

    Basic transactions API implementation

--- src/transactions/app.js
+++ src/transactions/app.js
@@ -1,10 +1,60 @@
 'use strict';
 
 const { DynamoRestfulHandler } = require('./DynamoRestfulHandler.js');
+const Ajv = require('ajv');
 
+const region = "eu-north-1";
 const tableName = "transactions";
 
 exports.handler = async (event, context) => {
-    const restHandler = new DynamoRestfulHandler(tableName);
+    const restHandler = new DynamoRestfulHandler(region, tableName, new PayloadValidators());
     return await restHandler.handleApiEvent(event);
 };
+
+class PayloadValidators {
+
+    constructor(){
+        this.ajv = new Ajv();
+    }
+
+    generatePutTransactionValidator() {
+        return this.ajv.compile({
+            type: "object",
+            properties: {
+                // id not allowed
+                /*timestamp: {
+                    type: "integer"
+                },*/
+                account_id: {
+                    type: "string", 
+                    pattern: "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$" // = UUID
+                },
+                transaction_amount: {
+                    type: "integer"
+                },
+                /*transaction_result: {
+                    type: "integer"
+                },*/
+                action: {
+                    enum: ["BUY_PRODUCT","WITHDRAW","DEPOSIT","CANCEL","INCONSISTENCY_CORRECTION"]
+                },
+                authentication: {
+                    enum: ["USERNAME_PASSWORD","MEDIA_PIN","MEDIA_ONLY"]
+                },
+                device_type: {
+                    enum: ["App", "PoS"]
+                },
+                device_name: {
+                    type: "string"
+                },
+                product: {
+                    type: "object"
+                }
+            },
+            required: ["account_id", "transaction_amount", "action", "authentication", "device_type", "device_name"]
+        })
+    }
+
+
+
+}
